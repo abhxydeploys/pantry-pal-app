064021f8670a35065af97fb291726ea9ad224338
Abhay Kumar <s.kumarabhay37@gmail.com>
2025-06-15 13:54:30 +0000

    fix all lint issues like earlier you did

--- pantrypalcodebase/src/index.ts
+++ pantrypalcodebase/src/index.ts
@@ -1,4 +1,3 @@
-
 /**
  * Import function triggers from their respective submodules:
  *
@@ -7,6 +6,9 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
+
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
